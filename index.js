2405724d692d0997ca038dc4a8ee51b1f9783fd4
Evan Sosenko <evan.sosenko@meltwater.com>
2020-02-11 11:34:15 -0800

    Export apollo errors from apollo-server-core

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ export { default as gql } from 'graphql-tag'
 export * from 'graphql-tools'
 export * from 'graphql'
 export * from './lib'
+export {
+  ApolloError,
+  AuthenticationError,
+  ForbiddenError,
+  SyntaxError,
+  UserInputError,
+  ValidationError,
+  formatApolloErrors,
+  toApolloError
+} from 'apollo-server-core'
